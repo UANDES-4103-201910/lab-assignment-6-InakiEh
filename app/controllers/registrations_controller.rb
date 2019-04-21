60cc55dcb798373dbfa7ee6564ab9eefe9ab85b7
Iñaki <ierrazuriz@miuandes.cl>
2019-04-20 23:51:23 -0400

    Lab 6 Reg

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,17 @@ class RegistrationsController < ApplicationController
 	end
 
 	def create
-	    #complete this method
+	    @user = User.new(req_user_params)
+		if @user.save
+			flash[:notice] = "Uswr registered"
+			redirect_to root_path
+		else
+			flash[:notice] = "Registration error "
+			redirect_to sign_in_path
+		end
+	end
+
+	def req_user_params
+		params.require(:registrations).permit(:name, :last_name, :email, :password, :phone)
 	end
 end
